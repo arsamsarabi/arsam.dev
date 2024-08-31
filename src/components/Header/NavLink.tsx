cadbdc597914d524a12c8b522df9c62cd6de7f50
Arsam Sarabi <arsamsarabi@me.com>
2024-08-31 20:05:14 +0100

    NavLink indicator animation

--- src/components/Header/NavLink.tsx
+++ src/components/Header/NavLink.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Icon, IconNamesType } from '#/components/Icon'
@@ -19,15 +20,20 @@ export const NavLink = ({ text, link, icon }: NavLinkProps) => {
     <Link
       href={link}
       className={cn(
-        'flex items-center justify-center gap-2 px-2 py-1 transition-all hover:text-brand-primary-dark',
+        'relative flex items-center justify-center gap-2 px-2 py-1 transition-all hover:text-brand-primary-dark',
         {
-          'pointer-events-none rounded border-b-2 border-brand-primary font-bold text-brand-primary':
-            isActive,
+          'pointer-events-none font-bold text-brand-primary': isActive,
         }
       )}
     >
       <Icon name={icon} />
       {text}
+      {isActive && (
+        <motion.div
+          layoutId="underline"
+          className="absolute bottom-[-1px] left-[50%] right-[50%] h-[6px] w-[6px] rounded-full bg-brand-primary"
+        />
+      )}
     </Link>
   )
 }
