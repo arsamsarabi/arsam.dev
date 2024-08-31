0b6a5e3b8a9f7743483ad8edb72015a34c2032c3
Arsam Sarabi <arsamsarabi@me.com>
2024-08-31 18:55:40 +0100

    hero test responsive changes

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
       <Header className="fixed left-0 right-0 top-0 z-50 md:bg-transparent" />
 
       <div className="center-child w-full flex-1">
-        <div className="flex w-full flex-col items-center justify-around gap-4 px-8 md:w-5/6 md:px-0 xl:flex-row">
+        <div className="flex w-full flex-col items-center justify-around gap-4 px-8 md:w-5/6 md:px-0 xl:flex-row xl:gap-8">
           <div className="relative h-56 w-44 xl:w-[175px]">
             <Image
               src="/images/arsam.JPG"
@@ -18,7 +18,7 @@ export const Hero = () => {
               priority
             />
           </div>
-          <div className="flex-1">
+          <div className="flex flex-1 flex-col lg:gap-2">
             <p className="text-center font-heading text-4xl font-bold text-brand-primary-dark lg:text-5xl xl:text-4xl 2xl:text-5xl">
               Software Engineer
             </p>
